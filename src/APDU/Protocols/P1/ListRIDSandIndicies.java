dba531b595dba4c7ec4737f969d90f73ffa20604
ElliotAll <darkbear04078(@gmail.com>
2018-03-19 19:22:03 +0300

    1st commitment

---
+++ src/APDU/Protocols/P1/ListRIDSandIndicies.java
@@ -0,0 +1,16 @@
+package APDU.Protocols.P1;
+
+import Constructor.SerialWriteClass;
+
+import static APDU.Protocols.P1.ListRIDS.*;
+
+public class ListRIDSandIndicies {
+    public static void main() {
+        SerialWriteRIDS.main(List1);
+        SerialWriteRIDS.main(List2);
+        SerialWriteRIDS.main(List3);
+        SerialWriteRIDS.main(List4);
+        SerialWriteRIDS.main(List5);
+        SerialWriteRIDS.main(List6);
+    }
+}
